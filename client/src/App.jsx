e45ab6802a95cbb6c566349ca930913ef719cdac
ppriyam7 <priyanshupriyam610@gmail.com>
2024-05-29 13:42:30 +0530

    updated all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 import Signup from "./pages/Signup";
-import Dashboard from "./pages/dashboard";
-import Projects from "./pages/projects";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/home" element={<Home />} />;
+        <Route path="/about" element={<About />} />;
+        <Route path="/sign-in" element={<Signin />} />;
+        <Route path="/dashboard" element={<Dashboard />} />;
+        <Route path="/projects" element={<Projects />} />;
         <Route path="/sign-up" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   );
