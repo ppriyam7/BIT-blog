e248aab46b8b5504119baf9e9b52b3f62f39c522
ppriyam7 <priyanshupriyam610@gmail.com>
2024-06-03 21:39:43 +0530

    added header

--- api/index.js
+++ api/index.js
@@ -1,4 +1,10 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose.connect(
+  "mongodb+srv://priyanshu:qwerty1234@@bitblog.s7rwmfh.mongodb.net/bitblog?retryWrites=true&w=majority"
+);
+
 const app = express();
 app.listen(3000, () => {
   console.log("Server running on port 3000!");
