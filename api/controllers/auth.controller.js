a7e5d663958bc7ef47e1ec7519be45f4916b6e3d
ppriyam7 <priyanshupriyam610@gmail.com>
2024-06-05 00:29:13 +0530

    created sign up api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import { createRequire } from "module";
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
 const require = createRequire(import.meta.url);
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
@@ -13,7 +15,12 @@ export const signup = async (req, res) => {
   ) {
     return res.status(400).json({ message: "All fields are required" });
   }
-  const newuser = new User({ username, email, password });
-  await newuser.save();
-  res.json({ message: "Sign-up successful" });
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newuser = new User({ username, email, password: hashedPassword });
+  try {
+    await newuser.save();
+    res.json({ message: "Sign-up successful" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
