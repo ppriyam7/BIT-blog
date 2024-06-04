08ad3193580e65bc4518534bf0bcd4c994e5b1ae
ppriyam7 <priyanshupriyam610@gmail.com>
2024-06-05 00:11:57 +0530

    postman api tested

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,19 @@
 import { createRequire } from "module";
+import User from "../models/user.model.js";
 const require = createRequire(import.meta.url);
 export const signup = async (req, res) => {
-  console.log(req.body);
+  const { username, email, password } = req.body;
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  const newuser = new User({ username, email, password });
+  await newuser.save();
+  res.json({ message: "Sign-up successful" });
 };
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
 
 mongoose
@@ -17,6 +18,7 @@ mongoose
     console.log(err);
   });
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server running on port 3000!");
 });
