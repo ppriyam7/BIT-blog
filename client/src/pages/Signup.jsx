87a141f19330061f218b7ef9146b9baf93472a7e
ppriyam7 <priyanshupriyam610@gmail.com>
2024-07-06 12:31:35 +0530

    signup updated

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -27,7 +27,7 @@ export default function Signup() {
     try {
       setLoading(true);
       setErrorMessage(null);
-      const res = await fetch("http://localhost:3000/api/auth/signup", {
+      const res = await fetch("/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
